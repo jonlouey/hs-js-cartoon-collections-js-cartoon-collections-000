9ebb923eff7cfdd7069742e66a32ff908c0f9c98
adventurous-key-5922 <louey.jonathan@gmail.com>
2019-03-26 02:07:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // 1. Create a function that will accept an array called "dwarves"
-// 2. Create an empty array variable to house a modefied list (list_of_dwarves)
+// 2. Create an empty array variable that will house the modefied list (list_of_dwarves) you will be creating. 
 // 3. Loop through "dwarves" array. 
-// 4. Add a number and a period in front of each name
-// 5. Return back the new array (list_of_dwarves)
+// 4. Add the correct number and a period in front of each name. 
+// 5. During each iteration add a dwarf to list_of_dwarves (+=). 
+// 5. Return back the new array (list_of_dwarves). 
 
 function dwarfRollCall(dwarves) {
   var list_of_dwarves = ""; 
   for (var i = 0; i < dwarves.length; i++){
-    list_of_dwarves += `${i + 1}. ${dwarves[i]} `
+    list_of_dwarves += `${i + 1}. ${dwarves[i]} `; 
   }
   return list_of_dwarves; 
 } 
 
-// 1. Create a function that accpets an array.
-// 2. Create a variable that will house the updated array. 
+// 1. Create a function that accpets an array called planeteer. 
+// 2. Create an empty array variable (elements). 
 // 3. Loop through planeteerCalls array. 
 // 4. Modify text in planteerCalls by making all characters capitalized and adding an exclamation mark to each string. 
-// 5. Push the modified text to new variable (elements)  
-// 6. Return elements 
-
-
+// 5. Push the modified text to new variable (elements).   
+// 6. Return elements.  
 
 function summonCaptainPlanet(planeteerCalls){
   var elements = []; 
@@ -29,12 +28,12 @@ function summonCaptainPlanet(planeteerCalls){
   return elements; 
 }
 
-// 1. Create function that accepts an Array.
-// 2. Create an array where we can input true and false text created in steps 3, 4, and 5. 
-// 3. Review each item in the array to count its length. 
-// 4. If any of the items in the array are longer than 4 characters return true. 
-// 5. Else return false 
-// 6. Review array and return true if "true" can be found in the array https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes. 
+// 1. Create function that accepts an array called planeteer calls.
+// 2. Create an empty array variable (short_long) where we can input true and false text created in steps 3, 4, and 5. 
+// 3. Review each item in the array. 
+// 4. If any of the items in the array are longer than 4 characters return true and push true to short_long.
+// 5. Else return false.  
+// 6. Review array (short_long) and return true if "true" can be found in the array (includes). https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes. 
 
 function longPlaneteerCalls(words) {
   var short_long = []
