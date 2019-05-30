8a2d93da510d432ddc6916f22a151f25f423e1e3
adventurous-key-5922 <louey.jonathan@gmail.com>
2019-05-30 02:05:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Goal: Update array so that all elements are listed in an ordered list. 
 // 1. Create a function that will accept an array called "dwarves"
 // 2. Create an empty array variable that will house the modefied list (list_of_dwarves) you will be creating. 
 // 3. Loop through "dwarves" array. 
@@ -6,13 +7,14 @@
 // 5. Return back the new array (list_of_dwarves). 
 
 function dwarfRollCall(dwarves) {
-  var list_of_dwarves = ""; 
-  for (var i = 0; i < dwarves.length; i++){
+  var list_of_dwarves = []; 
+  for (var i = 0; i < dwarves.length/2; i++){
     list_of_dwarves += `${i + 1}. ${dwarves[i]} `; 
   }
   return list_of_dwarves; 
 } 
 
+// Goal: Update array so that all elements in all caps and have an exclamation mark. 
 // 1. Create a function that accpets an array called planeteer. 
 // 2. Create an empty array variable (elements). 
 // 3. Loop through planeteerCalls array. 
@@ -28,6 +30,7 @@ function summonCaptainPlanet(planeteerCalls){
   return elements; 
 }
 
+// Goal: Identify if any of the elements in an array is longer than 4 characters long. 
 // 1. Create function that accepts an array called planeteer calls.
 // 2. Create an empty array variable (short_long) where we can input true and false text created in steps 3, 4, and 5. 
 // 3. Review each item in the array. 
@@ -39,15 +42,16 @@ function longPlaneteerCalls(words) {
   var short_long = []
   for(var r = 0; r < words.length; r++){
     if (words[r].length > 4) { 
-    short_long.push("true")
+    short_long.unshift("true")
   }
     else{
-    short_long.push("false")
+    short_long.unshift("false")
   }
 }
 return(short_long.includes("true"));
 }
 
+// Goal: Review array and return back first cheese name. If there is no cheese in the array return "No Cheese".
 // 1. Create a function that accepts an array called "food". 
 // 2. Review each item in the array <-- For loop 
 // 3. If Cheddar, Gouda, or Camembert 
@@ -71,7 +75,8 @@ function findTheCheese (foods) {
          return foods[c];
        } 
     }
-     if(foods[c] !== 'cheddar' || foods !== 'gouda' || foods !== 'camembert') {
+     if(foods[c] !== 'cheddar' || foods[c] !== 'gouda' || foods[c] !== 'camembert') {
        return 'no cheese!';
      } 
  }
+ 
